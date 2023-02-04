233d25c44e6e70fb1224f82ea2d8535b18083cab
lenzjo <lenzjo@gmail.com>
2023-02-04 17:17:45 +0000

    Initial Setup Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# RPi-AIV-Robot
+# RPi4 based Robot that uses Vision to Navigate
 Development of a robot based on the RPi 4 and using ros2 humble
+
+### Nodes
+- Publisher
+- Subscriber
---
+++ vision_rpi_bot/vision_rpi_bot/publisher.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Int16
+
+class MinimalPublisher(Node):
+
+  def __init__(self):
+    super().__init__('simple_publisher')
+    self.publisher_ = self.create_publisher(Int16, '/pub_topic', 10)
+    timer_period = 0.1 # seconds
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+    self.iterator = 0
+
+  def timer_callback(self):
+    msg = Int16()
+    msg.data = self.iterator
+    self.publisher_.publish(msg)
+    self.get_logger().info('Publishing: "%d"' % msg.data)
+    self.iterator += 10
+
+
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  minimal_publisher = MinimalPublisher()
+
+  rclpy.spin(minimal_publisher)
+
+  minimal_publisher.destroy_node()
+  rclpy.shutdown()
+
+
+if __name__ == '__main__':
+  main()
